fc772c9cec2f28623c61a2494fb92dfa830c6b30
Alessandro <alessandro.mancinitortora@par-tec.it>
2020-11-15 16:26:33 +0100

    revert di revert

--- esercizio_studenti_flask/view/login.py
+++ esercizio_studenti_flask/view/login.py
@@ -1,8 +1,7 @@
 from flask import render_template, flash, redirect, url_for, request, Blueprint
 from esercizio_studenti_flask.forms import Loginform
 from esercizio_studenti_flask import bcrypt
-from flask_login import login_user
-
+from flask_security import login_user
 
 from esercizio_studenti_flask.model import User
 
@@ -14,10 +13,10 @@ login_bp = Blueprint('login', __name__, template_folder='../templates')
 def login():
     form = Loginform()
     if form.validate_on_submit():
-        user = User.query.filter_by(name=form.name.data).first()
+        user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user)
-            flash(f'You logged in as {user.name}!', 'success')
+            flash(f'You logged in as {user.email}!', 'success')
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('home.home'))
         else:
