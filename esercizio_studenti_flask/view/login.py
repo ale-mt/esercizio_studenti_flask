43a033ceb77b3f60dfac230ee7ef9c4083ec43a7
ale-mt <alessandro.mancinitortora@par-tec.it>
2021-01-13 12:28:23 +0100

    removed flask-wtf for login

--- esercizio_studenti_flask/view/login.py
+++ esercizio_studenti_flask/view/login.py
@@ -2,6 +2,7 @@ from flask import render_template, flash, redirect, url_for, request, Blueprint
 from esercizio_studenti_flask.forms import Loginform
 from esercizio_studenti_flask import bcrypt
 from flask_security import login_user
+import logging
 
 from esercizio_studenti_flask.model import User
 
@@ -11,15 +12,20 @@ login_bp = Blueprint('login', __name__, template_folder='../templates')
 
 @login_bp.route('/login', methods=['POST', 'GET'])
 def login():
-    form = Loginform()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
+    if request.method == "POST":
+        logging.info("login post request")
+        email = request.form.get('email')
+        pswd = request.form.get('pswd')
+
+        print("dentro submit")
+
+        user = User.query.filter_by(email=email).first()
+        if user and bcrypt.check_password_hash(user.password, pswd):
             login_user(user)
             flash(f'You logged in as {user.email}!', 'success')
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('home.home'))
         else:
             flash('Login failed!', 'warning')
-    return render_template('login.html', title='Login', form=form)
 
+    return render_template('login.html', title='Login')
